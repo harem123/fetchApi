394fb2aa710079ad655581130890f9a10a747c30
harem123 <31325055+harem123@users.noreply.github.com>
2021-10-08 16:26:38 -0500

    first commit
    v1

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import FetchApi from './components/FetchApi'
+
+function App () {
+  return(
+    <div className = "App" >
+      desde main
+      <FetchApi/>
+    </div>
+  )
+}
+
+
+export default App
